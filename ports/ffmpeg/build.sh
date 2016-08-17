efff5c72c4ae65edeb71f4d32ecbb9eb97598edc
Scott Meisburger <smeisburger@gmail.com>
2016-08-17 19:50:12 +0000

    upgraded ffmpeg to 3.1.2
    don't build cmd line programs for ffmpeg
    added rtsp and open ssl options for ffmpeg

--- ports/ffmpeg/build.sh
+++ ports/ffmpeg/build.sh
@@ -28,18 +28,20 @@ ConfigureStep() {
   LogExecute ${SRC_DIR}/configure \
     --cross-prefix=${NACL_CROSS_PREFIX}- \
     --target-os=linux \
-    --disable-everything \
-    --enable-static \
     --enable-cross-compile \
+    --disable-programs \
+    --disable-doc \
+    --disable-everything \
     --enable-decoder=aac,h264,mjpeg,mpeg2video,mpeg4,pcm_mulaw \
     --enable-encoder=aac,mpeg4,mjpeg \
-    --enable-protocol=concat,file \
-    --enable-demuxer=aac,avi,h264,image2,matroska,pcm_s16le,pcm_mulaw,mov,m4v,rawvideo,wav \
+    --enable-protocol=concat,file,rtp,tls_openssl \
+    --enable-demuxer=aac,avi,h264,image2,matroska,pcm_s16le,pcm_mulaw,mov,m4v,rawvideo,wav,rtsp,mjpeg \
     --enable-muxer=h264,ipod,mov,mp4 \
     --enable-parser=aac,h264,mjpeg,mpeg4video,mpegaudio,mpegvideo,png \
     --enable-bsf=aac_adtstoasc \
     --enable-filter=transpose \
     --enable-avresample \
+    --enable-openssl \
     --prefix=${PREFIX} \
     ${extra_args}
 }
